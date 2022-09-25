aa1d43b59f880fce6c3ed38a1135d58d980b73f9
otabekJaval <112159831+otabekJaval@users.noreply.github.com>
2022-09-25 15:58:09 +0500

    Update HalfPyramid.java

--- src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
@@ -1,11 +1,23 @@
 package school.mjc.stage0.loops.finalTask;
 
 public class HalfPyramid {
+
     public void printHalfPyramid(int cathetusLength) {
 
+        for(int i=1;i<=cathetusLength;i++){
+            for(int j=0;j<cathetusLength;j++){
+                if(i+j>=cathetusLength)
+                    System.out.print("*");
+                else System.out.print(" ");
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
         new HalfPyramid().printHalfPyramid(8);
+
+
+
     }
 }
