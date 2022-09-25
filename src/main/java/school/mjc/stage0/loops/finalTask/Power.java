79be87ae2be9117236c9a3f145290ffd7e13f6a5
otabekJaval <112159831+otabekJaval@users.noreply.github.com>
2022-09-25 15:58:48 +0500

    Update Power.java

--- src/main/java/school/mjc/stage0/loops/finalTask/Power.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Power.java
@@ -3,6 +3,14 @@ package school.mjc.stage0.loops.finalTask;
 public class Power {
     public void printPower(int numberToPrint, int power) {
 
+        int sum=01;
+
+        for(int i=1;i<=power;i++){
+            sum*=numberToPrint;
+        }
+
+        System.out.println(sum);
+
     }
 
     public static void main(String[] args) {
