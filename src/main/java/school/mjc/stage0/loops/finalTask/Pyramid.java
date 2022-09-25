8fa3215c0405cf533266255f984716dd721d8838
otabekJaval <112159831+otabekJaval@users.noreply.github.com>
2022-09-25 15:59:17 +0500

    Update Pyramid.java

--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -3,9 +3,41 @@ package school.mjc.stage0.loops.finalTask;
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
 
+        int num = 5;
+        int x = 0;
+
+        // Outer loop for rows
+        for (int i = 1; i <= num; i++) {
+            x = i - 1;
+
+      
+            for (int j = i; j <= num - 1; j++) {
+
+      
+                System.out.print(" ");
+
+        
+                System.out.print("  ");
+            }
+
+      
+            for (int j = 0; j <= x; j++)
+                System.out.print((i + j) < 10
+                        ? (i + j) + "  "
+                        : (i + j) + " ");
+
+            for (int j = 1; j <= x; j++)
+                System.out.print((i + x - j) < 10
+                        ? (i + x - j) + "  "
+                        : (i + x - j) + " ");
+
+    
+            System.out.println();
+        }
+
     }
 
     public static void main(String[] args) {
-        new Pyramid().printPyramid(7);
+        new Pyramid().printPyramid(9);
     }
 }
